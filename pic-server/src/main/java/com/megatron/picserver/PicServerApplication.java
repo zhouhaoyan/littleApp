cfab68a7f173048ce6d76bd69503b31633e02f46
zhouhaoyan <ttzhyran@163.com>
2018-03-20 23:02:24 +0800

    服务端代码初始化
    初始化

---
+++ pic-server/src/main/java/com/megatron/picserver/PicServerApplication.java
@@ -0,0 +1,12 @@
+package com.megatron.picserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicServerApplication.class, args);
+	}
+}
