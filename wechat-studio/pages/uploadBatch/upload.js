b0bc9ce7a92aee2909a52f85dd6c40aa1b9519f4
zhouhaoyan <ttzhyran@!63.com>
2018-09-02 18:04:58 +0800

    添加多用户能力

--- wechat-studio/pages/uploadBatch/upload.js
+++ wechat-studio/pages/uploadBatch/upload.js
@@ -16,7 +16,6 @@ Page({
     name:"",
     title:"",
     description:""
-    
   },
   onLoad: function () {
     console.log('onLoad')
@@ -51,8 +50,9 @@ Page({
     }
      
     wx.chooseImage({
-      count:9,
-      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      count:20,
+      //sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
       success: function (res) {
         var filePathList = res.tempFilePaths;
@@ -102,7 +102,8 @@ Page({
    
     wx.chooseImage({
       count: 1,
-      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+    //  sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sizeType: [ 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
       success: function (res) {
         var filePathList = res.tempFilePaths;
